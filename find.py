83149a42f07fcc6412cca10ab98203ed2596eef1
Kaleb Zuckerman <kaleb_zuckerman@brown.edu>
2023-07-06 11:46:02 -0400

    fixed jellyfish hash size

--- find.py
+++ find.py
@@ -2,7 +2,7 @@ import sys
 import os
 import json
 
-def run_jellyfish(data, genome_fname=None, output_prefix=None, min=6, max=12):
+def run_jellyfish(data, seq_length, genome_fname=None, output_prefix=None, min=6, max=12):
     """
     Runs jellyfish program using the output_prefix and transfroms the kmer count information to txt files. Count k-mers within range 
     found in the JSON file.
@@ -14,7 +14,7 @@ def run_jellyfish(data, genome_fname=None, output_prefix=None, min=6, max=12):
     """
     for k in range(min, max+1, 1):
         if not os.path.exists(output_prefix+'_'+str(k)+'mer_all.txt'):
-            os.system("jellyfish count -m "+str(k) + " -s 1000000 -t " + str(data['cpus']) + " " + genome_fname + " -o " + output_prefix+'_'+str(k)+'mer_all.jf')
+            os.system("jellyfish count -m "+str(k) + " -s " + str(seq_length) + " -t " + str(data['cpus']) + " " + genome_fname + " -o " + output_prefix+'_'+str(k)+'mer_all.jf')
             os.system("jellyfish dump -c " + output_prefix+'_'+str(k)+'mer_all.jf' + " > " + output_prefix+'_'+str(k)+'mer_all.txt')
         if os.path.exists(output_prefix+'_'+str(k)+'mer_all.jf'):
             os.system("rm " + output_prefix+'_'+str(k)+'mer_all.jf')
@@ -29,13 +29,13 @@ def step1(data, fg_prefixes, fg_genomes, bg_prefixes, bg_genomes, min, max):
 
     print("Running jellyfish for foreground...")
     for i, fg_prefix in enumerate(fg_prefixes):
-        run_jellyfish(data, fg_genomes[i], fg_prefix, min, max)
+        run_jellyfish(data, data['fg_seq_lengths'][i], fg_genomes[i], fg_prefix, min, max)
 
     print("Running jellyfish for background...")
     for i, bg_prefix in enumerate(bg_prefixes):
         for k in range(min, max+1, 1):
             if not os.path.exists(bg_prefix+'_'+str(k)+'mer_all.jf'):
-                os.system("jellyfish count -m "+str(k) + " -s 1000000 -t " + str(data['cpus']) + " " + bg_genomes[i] + " -o " + bg_prefix+'_'+str(k)+'mer_all.jf')
+                os.system("jellyfish count -m "+str(k) + " -s " + str(data['bg_seq_lengths'][i]) + " -t " + str(data['cpus']) + " " + bg_genomes[i] + " -o " + bg_prefix+'_'+str(k)+'mer_all.jf')
 
     print("Done running jellyfish")
 
